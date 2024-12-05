6e7bf33032d1a241af7c54520667f667d058ddd0
JeanFreitas <jean.sistemas@lar.ind.br>
2024-12-05 16:59:14 -0300

    Add GarantiasOferecidasFieldsetComponent to AnaliseForm
    Introduce the GarantiasOferecidasFieldsetComponent to enhance the form with fields specific to offered guarantees. This new component displays a structured table layout for entering detailed guarantee information.

---
+++ Components/Pages/Components/Analise/GarantiasOferecidasFieldsetComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Front.Components.Pages.Components.Analise;
+
+public partial class GarantiasOferecidasFieldsetComponent : ComponentBase
+{
+    private DadosCredito dados = new DadosCredito();
+}
+
+internal class DadosCredito
+{
+}
