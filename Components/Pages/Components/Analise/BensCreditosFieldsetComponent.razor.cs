3ef91a8d039a06a991721ca4c674b90ff2dbd40e
JeanFreitas <jean.sistemas@lar.ind.br>
2024-12-09 11:56:07 -0300

    Add employee data grid to BensCreditosFieldsetComponent
    Integrated a RadzenDataGrid to display employee data within the BensCreditosFieldsetComponent. Initialized an IEnumerable of employees in the component's constructor. Implemented styling and data binding for enhanced UI presentation.

--- Components/Pages/Components/Analise/BensCreditosFieldsetComponent.razor.cs
+++ Components/Pages/Components/Analise/BensCreditosFieldsetComponent.razor.cs
@@ -1,7 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components;
+using RadzenBlazorDemos.Models.Northwind;
 
 namespace Front.Components.Pages.Components.Analise;
 
 public partial class BensCreditosFieldsetComponent : ComponentBase
 {
+    IEnumerable<Employee> _employees;
+
+    public BensCreditosFieldsetComponent()
+    {
+        _employees = Enumerable.Empty<Employee>();
+    }
 }
