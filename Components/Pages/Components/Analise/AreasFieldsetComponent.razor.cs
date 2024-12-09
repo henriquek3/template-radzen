5ebece6a8288333ad6b2ed872e24f5af3bce8248
JeanFreitas <jean.sistemas@lar.ind.br>
2024-12-09 10:16:37 -0300

    Enhance AreasFieldsetComponent UI layout
    Improved the UI layout within AreasFieldsetComponent for better structure and readability. Added detailed sections to display property data, including columns for type, total area, cultivable percentage, and predominant soil. This update enhances clarity and usability for users interacting with the component.

--- Components/Pages/Components/Analise/AreasFieldsetComponent.razor.cs
+++ Components/Pages/Components/Analise/AreasFieldsetComponent.razor.cs
@@ -4,4 +4,11 @@ namespace Front.Components.Pages.Components.Analise;
 
 public partial class AreasFieldsetComponent : ComponentBase
 {
+    private List<string> soloPredominante = new()
+    {
+        "Argiloso",
+        "Argilo-Arenoso",
+        "Arenoso",
+        "Arenoso-Argiloso"
+    };
 }
